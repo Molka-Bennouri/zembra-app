fc18bdafa0afa643079d311477b3f59e27c28cb4
Mariem Rezgui <rezguimariem141@gmail.com>
2026-03-16 12:18:20 +0100

    Invoices

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ListingPage from './listing/ListingPage';
 import ProfilePage from "./profile/ProfilePage";
 import ManagePayment from './payment/ManagePayment';
 import PaymentHistory from './payment/PaymentHistory';
-
+import Invoices from './payment/Invoices';
 function App() {
   return (
     <Router>
@@ -17,8 +17,10 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/payment" element={<ManagePayment />} />
         <Route path="/PaymentHistory" element={<PaymentHistory />} />
-        <Route path="/login" element={<Login/>} />
-         <Route path="/signup" element={<Signup/>} />
+        <Route path="/Invoices" element={<Invoices />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/PaymentHistory" element={<PaymentHistory />} />
       </Routes>
     </Router>
   );
